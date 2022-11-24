6de6ffbc3b102c0435b8a314697dfd1d641ced1a
장진혁/Client개발팀/원스토어 <ironelder@onestorecorp.com>
2022-11-24 16:12:33 +0900

    [ironelder] memory composition

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -41,7 +41,9 @@ fun WellnessScreen(modifier: Modifier = Modifier) {
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count = 0
+        var count by remember {
+            mutableStateOf(0)
+        }
         Text(text = "You've had $count glasses.")
         Button(onClick = { count++ }, modifier = Modifier.padding(8.dp)) {
             Text(text = "Add one")
